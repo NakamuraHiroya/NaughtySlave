5ac2724e8213c3d710b06b21c742126474a27851
NakamuraHiroya <hiroya19840513@gmail.com>
2017-08-26 17:21:15 +0900

    Profileで一通りCSVファイル読み込んどくことにした

--- Documents/WindowsPowershell/Microsoft.Powershell_profile.ps1
+++ Documents/WindowsPowershell/Microsoft.Powershell_profile.ps1
@@ -17,8 +17,10 @@ Add-Type -AssemblyName System.Windows.Forms
 Set-variable -name HOME_DIR -value C:\Users\hiroya\ -option constant;if (-not(Test-Path $HOME_DIR)) {"$HOME_DIR not exist"}
 Set-variable -name LOG_DIR -value $HOME_DIR"Log\" -option constant	;if (-not(Test-Path $LOG_DIR)) {"$LOG_DIR not exist"}
 Set-variable -name FUNC_DIR -value $HOME_DIR"Func\" -option constant	;if (-not(Test-Path $FUNC_DIR)) {"$FUNC_DIR not exist"}
+Set-variable -name DOC_DIR -value $HOME_DIR"Documents\" -option constant;if(-not(Test-Path $DOC_DIR)) {"$DOC_DIR not exist"}
 Set-variable -name TABLE_DIR -value $HOME_DIR"Documents\My Data Sources\" -option constant;if(-not(Test-Path $TABLE_DIR)) {"$TABLE_DIR not exist"}
 Set-variable -name BUSINESS_DIR -value $HOME_DIR"Business\" -option constant;if(-not(Test-Path $TABLE_DIR)) {"$BUSINESS_DIR not exist"}
+
 
 # 日付系定数定義
 Set-variable -name YYYYMM -value (Get-Date).ToString("yyyyMM") -option constant
@@ -28,17 +30,23 @@ Set-variable -name YYYYMMDD -value (Get-Date).ToString("yyyyMMdd") -option const
 Set-variable -name LOG -value $LOG_DIR$yyyyMMdd"_log.txt" -option constant
 
 <#-------------------------------------
-[1-3. Alias定義]
--------------------------------------#>
-ipcsv $FUNC_DIR/ExeList.csv -Encoding Default|%{Set-Alias $_.AliasCode $_.ExeCode}
-
-<#-------------------------------------
-[1-4. グローバル関数定義]
+[1-3. グローバル関数定義]
 -------------------------------------#>
 ls $FUNC_DIR*.ps1 -name|%{. $FUNC_DIR$_}
 
+<#-------------------------------------
+[1-4. CSVファイル読み込み]
+-------------------------------------#>
+ls -name $FUNC_DIR*.csv|%{$CSV+=@{$_=(OpenCSVFile (Join-Path $FUNC_DIR $_))}}
+ls -name $TABLE_DIR*.csv|%{$CSV+=@{$_=(OpenCSVFile (Join-Path $TABLE_DIR $_))}}
+
+<#-------------------------------------
+[1-5. Alias定義]
+-------------------------------------#>
+$csv["ExeList.csv"]|%{if($_.AliasCode){Set-Alias $_.AliasCode $_.ExeCode}}
+
 <#-----------------------------------------------------------------------------
-[1. Prepare]
+[3. Custom]
 -----------------------------------------------------------------------------#>
 <#-------------------------------------
 [3-1. ビープ音消す]
