ff7c185d7a133c254506080506ff4da8abb56e59
NakamuraHiroya <hiroya19840513@gmail.com>
2017-09-21 00:18:55 +0900

    Add files via upload

---
+++ Func/1.Secretary.ps1
@@ -0,0 +1,16 @@
+function global:Secretary{
+
+param(
+	$ThisDir=$HOME_DIR
+)
+
+start .
+
+#ProjectListçÏê¨
+if(-not(test-path (Join-path $NS_DIR ProjectList.csv))){Set-Content $NS_DIR/ProjectList.csv "DivisionCode,ProjectCode,PathCode" -Encoding Default;ls $NS_DIR|?{$_.Attributes -like "Directory"}|%{$_.Parent.Name+","+$_.Name+","+$_.FullName}|Out-File $NS_DIR/ProjectList.csv -Append -Encoding default}
+
+#ç°ì˙ÇÃÇ‚ÇÈÇ±Ç∆Çï\é¶ÇµÇƒÇ≠ÇÍÇÈ
+$Data["TODO.csv"]
+
+}
+
