fbeb16b4f78ea4a64db127b4037da20f71b8e618
NakamuraHiroya <hiroya19840513@gmail.com>
2017-09-22 07:23:22 +0900

    Add files via upload

--- Func/1.Secretary.ps1
+++ Func/1.Secretary.ps1
@@ -1,16 +1,18 @@
 function global:Secretary{
 
-param(
-	$ThisDir=$HOME_DIR
-)
+cd $HOME_DIR
 
 start .
 
 #ProjectListçÏê¨
-if(-not(test-path (Join-path $NS_DIR ProjectList.csv))){Set-Content $NS_DIR/ProjectList.csv "DivisionCode,ProjectCode,PathCode" -Encoding Default;ls $NS_DIR|?{$_.Attributes -like "Directory"}|%{$_.Parent.Name+","+$_.Name+","+$_.FullName}|Out-File $NS_DIR/ProjectList.csv -Append -Encoding default}
+#if(-not(test-path (Join-path $NS_DIR ProjectList.csv))){Set-Content $NS_DIR/ProjectList.csv "DivisionCode,ProjectCode,PathCode" -Encoding Default;ls $NS_DIR|?{$_.Attributes -like "Directory"}|%{$_.Parent.Name+","+$_.Name+","+$_.FullName}|Out-File $NS_DIR/ProjectList.csv -Append -Encoding default}
 
 #ç°ì˙ÇÃÇ‚ÇÈÇ±Ç∆Çï\é¶ÇµÇƒÇ≠ÇÍÇÈ
 $Data["TODO.csv"]
+
+#WBS
+ls */*/*/ticketlist.csv|%{$TicketList+=OpenCSVFile $_.FullName};$TicketList|Export-CSV WBS.csv -encoding default -notypeinformation
+
 
 }
 
