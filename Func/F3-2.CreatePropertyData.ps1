724dc4db9eea9aeae6d9c4232bdf05aa7f7a8418
NakamuraHiroya <hiroya19840513@gmail.com>
2017-11-19 20:37:13 +0900

    Add files via upload

--- Func/F3-2.CreatePropertyData.ps1
+++ Func/F3-2.CreatePropertyData.ps1
@@ -7,13 +7,14 @@ param(
 	$OutFile=$("_"+($InFile -split "\.")[0]+"_Property.csv")
 )
 if(-not(Test-Path $OutFile)){
-	Set-Content $OutFile "InFile,Property,Key,Type,Min,Max,Order" -Encoding Default
+	Set-Content $OutFile "InFile,Property,Order,Key,Type,Min,Max" -Encoding Default
 	
 	(cat $InFile)[0]|%{$Header=$_ -split ","}
 	(cat $Infile)[1]|%{$SampleData=$_ -split ","}
 	
 	$i=0
-	$Header|%{$Infile+","+$Header[$i]+","+" "+","+((GuessValueType $Sampledata[$i]).gettype().name);$i++}|
+	
+	$Header|%{$Infile+","+$Header[$i]+","+($i+1)+","+" "+","+((GuessValueType $Sampledata[$i]).gettype().name);$i++}|
 	Out-file $OutFile -Encoding Default -Append
 
 }else{$Outfile +" :Already Exist";exit}
