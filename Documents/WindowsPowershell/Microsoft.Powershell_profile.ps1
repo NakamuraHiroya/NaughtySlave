6e04ee470d7c7b55292deb76c1cf3cb3e468fab4
NakamuraHiroya <hiroya19840513@gmail.com>
2017-11-26 12:45:46 +0900

    Add files via upload

--- Documents/WindowsPowershell/Microsoft.Powershell_profile.ps1
+++ Documents/WindowsPowershell/Microsoft.Powershell_profile.ps1
@@ -1,35 +1,34 @@
-#ã‚¢ã‚»ãƒ³ãƒ–ãƒªèª­ã¿è¾¼ã¿
+#ƒAƒZƒ“ƒuƒŠ“Ç‚İ‚İ
 Add-Type -AssemblyName System.Windows.Forms
 
-#å®šæ•°å®šç¾©
+#’è”’è‹`
 Set-variable -name HOME_DIR -value $HOME"\Documents\" -option constant;if (-not($HOME_DIR)) {"$HOME_DIR not exist"}
 Set-variable -name NS_DIR -value $HOME_DIR"NaughtySlave\" -option constant	;if (-not($NS_DIR)) {"$NS_DIR not exist"}
 Set-variable -name TABLE_DIR -value $HOME_DIR"My Data Sources\" -option constant;if(-not($TABLE_DIR)) {"$TABLE_DIR not exist"}
 
-# æ—¥ä»˜ç³»å®šæ•°å®šç¾©
+# “ú•tŒn’è”’è‹`
 Set-variable -name YYYYMM -value (Get-Date).ToString("yyyyMM") -option constant
 Set-variable -name YYYYMMDD -value (Get-Date).ToString("yyyyMMdd") -option constant
 
-#é–¢æ•°èª­ã¿è¾¼ã¿
+#ŠÖ”“Ç‚İ‚İ
 ls $NS_DIR*.ps1 -name|%{. $NS_DIR$_}
 
-#CSVãƒ•ã‚¡ã‚¤ãƒ«èª­ã¿è¾¼ã¿
+#DataSet
 ls -name $NS_DIR*.csv|%{$Data+=@{$_=(OpenCSVFile (Join-Path $NS_DIR $_))}}
-ls -name $HOME_DIR*.csv|%{$Data+=@{$_=(OpenCSVFile (Join-Path $HOME_DIR $_))}}
-ls -name $TABLE_DIR*.csv|%{$Data+=@{$_=(OpenCSVFile (Join-Path $TABLE_DIR $_))}}
+$data.'WBS.csv'|?{$_.SetData}|%{pushd $_.WorkDir;ls -name *.csv|%{$Data+=@{$_=(OpenCSVFile $_)}};popd}
 
-#Aliaså®šç¾©
-$Data["ExeList.csv"]|%{if($_.AliasCode){Set-Alias $_.AliasCode $_.ExeCode}}
+#Alias’è‹`
+$Data["AppList.csv"]|%{if($_.AliasCode){Set-Alias $_.AliasCode $_.ExeCode}}
 
-#Personaå®šç¾©
+#Persona’è‹`
 $JPCode=$data["Persona.csv"].JPCode
 $NPCode=$data["Persona.csv"].NPCode
 
-#ãƒ“ãƒ¼ãƒ—éŸ³æ¶ˆã™
+#ƒr[ƒv‰¹Á‚·
 # Stop-Service beep
 
-#Displayã‚¢ãƒ©ãƒ¼ãƒˆæ¶ˆã™
+#DisplayƒAƒ‰[ƒgÁ‚·
 (New-Object -ComObject Excel.Application).DisplayAlerts=$false
 
-#HOMEç§»å‹•
+#HOMEˆÚ“®
 cd $HOME_DIR
