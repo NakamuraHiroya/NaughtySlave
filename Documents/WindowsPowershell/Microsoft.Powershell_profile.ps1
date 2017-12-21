f23f31ef315beb208483dd2282704fe0b1a9d2e9
NakamuraHiroya <hiroya19840513@gmail.com>
2017-12-22 00:48:12 +0900

    Add files via upload

--- Documents/WindowsPowershell/Microsoft.Powershell_profile.ps1
+++ Documents/WindowsPowershell/Microsoft.Powershell_profile.ps1
@@ -1,28 +1,31 @@
 #アセンブリ読み込み
 Add-Type -AssemblyName System.Windows.Forms
 
-#定数定義
-Set-variable -name HOME_DIR -value $HOME"\Documents\" -option constant;if (-not($HOME_DIR)) {"$HOME_DIR not exist"}
-Set-variable -name NS_DIR -value $HOME_DIR"NaughtySlave\" -option constant	;if (-not($NS_DIR)) {"$NS_DIR not exist"}
-Set-variable -name TABLE_DIR -value $HOME_DIR"My Data Sources\" -option constant;if(-not($TABLE_DIR)) {"$TABLE_DIR not exist"}
-
 # 日付系定数定義
 Set-variable -name YYYYMM -value (Get-Date).ToString("yyyyMM") -option constant
 Set-variable -name YYYYMMDD -value (Get-Date).ToString("yyyyMMdd") -option constant
 
 #関数読み込み
+$NS_DIR="C:\Users\hiroya\Documents\NaughtySlave\"
 ls $NS_DIR*.ps1 -name|%{. $NS_DIR$_}
 
+#Databaselist作成
+$TABLE_DIR="C:\Users\hiroya\Documents\My Data Sources\"
+CreateDirInfo -WorkDir $TABLE_DIR -OutFile "DataBaseList.csv"
+cd $TABLE_DIR
+$DataBaseList=ipcsv DataBaseList.csv -Encoding Default
+$DataBaseList|%{createdirinfo -workdir $_.workdir -OutFile "_TableList.csv"}
+ls -dir -name|%{$DataBaseList+=(ipcsv (join-path $_ _TableList.csv) -Encoding Default)}
+$DataBaseList|Export-Csv DataBaseList.csv -Encoding Default -NoTypeInformation
+ls -name *.csv -Exclude _*|%{$DataList+=@{$_=(ipcsv $_ -Encoding Default)}}
+
 #DataSet
-ls -name $NS_DIR*.csv|%{$DataList+=@{$_=(OpenCSVFile (Join-Path $NS_DIR $_))}}
-$DataList.'WBS.csv'|?{$_.SetData}|%{pushd $_.WorkDir;ls -name *.csv|%{$DataList+=@{$_=(OpenCSVFile $_)}};popd}
+ls -name $NS_DIR*.csv -Exclude _*|%{$DataList+=@{$_=(ipcsv (Join-Path $NS_DIR $_) -Encoding Default)}}
+#$DataList.'WBS.csv'|?{$_.SetData}|%{pushd $_.WorkDir;ls -name *.csv -Exclude _*|%{$DataList+=@{$_=(ipcsv $_ -Encoding Default)}};popd}
+$DataList.'DatabaseList.csv'|%{pushd $_.WorkDir;ls -name *.csv -Exclude _*|%{$DataList+=@{$_=(ipcsv $_ -Encoding Default)}};popd}
 
 #Alias定義
 $DataList.'AppList.csv'|%{if($_.AliasCode){Set-Alias $_.AliasCode $_.ExeCode}}
-
-#Persona定義
-$JPCode=$DataList."Persona.csv".JPCode
-$NPCode=$dataList."Persona.csv".NPCode
 
 #ビープ音消す
 # Stop-Service beep
@@ -30,5 +33,3 @@ $NPCode=$dataList."Persona.csv".NPCode
 #Displayアラート消す
 (New-Object -ComObject Excel.Application).DisplayAlerts=$false
 
-#HOME移動
-cd $HOME_DIR
