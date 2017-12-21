4cb470bb60ab901b5940504e3a2e8280a3589ca3
NakamuraHiroya <hiroya19840513@gmail.com>
2017-12-21 23:25:14 +0900

    Add files via upload

--- Func/F2-1_XLSFileToCSVFile.ps1
+++ Func/F2-1_XLSFileToCSVFile.ps1
@@ -4,7 +4,7 @@ param(
 	$InFile=$(ls -name *.xls*|ogv -PassThru -Title "入力するファイルを指定して下さい。")
 )
 
-$excel = New-Object -ComObject Excel.Application
+$excel=New-Object -ComObject Excel.Application
 $excel.Visible=$false 
 $excel.DisplayAlerts=$false
 
@@ -13,9 +13,13 @@ $book=$excel.Workbooks.Open((Convert-path $InFile))
 
 foreach($sheet in $book.sheets){
 	$sheet.Activate()
-	$CSVFileName=(($InFile -split "\.")[0])+"_"+$sheet.Name+".csv"
-	#Excelを同じフォルダに新規CSV保存する
-	$book.SaveAs((Split-Path(Convert-Path $InFile) -parent) + "\" + $CSVFileName,6)
+	$OutFile=(($InFile -split "\.")[0])+"_"+$sheet.Name+".csv"
+
+	#Sheet数が1の場合そのままの名称でCSV保存
+	if((($book.sheets|measure).count) -eq 1){$OutFile=(($InFile -split "\.")[0])+".csv"}
+
+	#Excelを同じフォルダに新規CSV保存
+	$book.SaveAs((Split-Path(Convert-Path $InFile) -parent) + "\" + $OutFile,6)
 
 }
 $excel.Quit();$excel = $null
@@ -23,6 +27,4 @@ $excel.Quit();$excel = $null
 # チケットログ作成
 CreateTicketFile -data "XLSFileToCSVFile,$InFile"
 
-
 }
-
