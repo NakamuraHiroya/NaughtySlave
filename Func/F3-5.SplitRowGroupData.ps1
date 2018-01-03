c60a8407f212a570ff570d0a15f323a94644785b
NakamuraHiroya <hiroya19840513@gmail.com>
2018-01-03 11:39:05 +0900

    Add files via upload

--- Func/F3-5.SplitRowGroupData.ps1
+++ Func/F3-5.SplitRowGroupData.ps1
@@ -1,17 +1,22 @@
 function global:SplitRowGroupData{
 
 param(
-	$InFile=$(ls -name *.csv|ogv -PassThru -Title "入力するファイルを指定して下さい。"),
+	$InFile=$(ls -name *.csv -Exclude _*|ogv -PassThru -Title "入力するファイルを指定して下さい。"),
 	$Data=$(ipcsv $Infile -Encoding Default),
 	$raw=$((cat $InFile)[0] -split ","|ogv -PassThru -Title "分割対象の列名を選択して下さい。"),
-	$OutFile=$(($InFile -split "\.")[0])
+	$OutFile=$(($InFile -split "\.")[0]),
+	$Ticket
 )
 
 # 【】や[]や/を変換しないとダメ！
 $data|group $raw|%{$name=$_.Name;$_.Group|Export-CSV ($OutFile+"_"+$name+".csv") -Encoding Default -NoTypeInformation}
 
+# ダブルクオーテーションTrim
+$data|group $raw|%{TrimFile ($OutFile+"_"+$_.Name+".csv")}
+
+
 # チケットログ作成
-CreateTicketData -data "SplitRowGroupData,$InFile,$OutFile,$Data,$raw"
+if($Ticket){CreateTicketData -data "$Ticket,SplitRowGroupData,$InFile,$OutFile,$Data,$raw"}
 
 
 }
