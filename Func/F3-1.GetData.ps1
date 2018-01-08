b74eb09c3116804698d98f859f1b58621489000a
NakamuraHiroya <hiroya19840513@gmail.com>
2018-01-09 00:39:31 +0900

    Add files via upload

--- Func/F3-1.GetData.ps1
+++ Func/F3-1.GetData.ps1
@@ -3,13 +3,15 @@ function global:GetData{
 param(
 	$Data=$($DataList.($DataList.Keys|sort|ogv -PassThru -Title "取得するデータを選択して下さい。")),
 	$Line=$($Data|ogv -PassThru -Title "取得する行を指定して下さい。"),
-	$Row=$($Data|get-member|?{$_.MemberType -eq "NoteProperty"}|%{$_.Name}|ogv -PassThru -Title "取得する列を指定して下さい。")
-
+	$Row=$($Data|get-member|?{$_.MemberType -eq "NoteProperty"}|%{$_.Name}|ogv -PassThru -Title "取得する列を指定して下さい。"),
+	$OutFile,
+	$Ticket
 )
 
 $Line|select $Row
 
+if($OutFile){$Line|select $Row|Export-csv $OutFile -Encoding Default -NoTypeInformation}
 
-CreateTicketData "GetValue,-,-,$Data,$Row,$Line"
+if($Ticket){CreateTicketData -data "$Ticket,GetData,-,$OutFile,$Data,$Row,$Line"}
 
 }
