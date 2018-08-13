301fc1f17fa04ce336d5420756d1f6f4f0c7743a
NakamuraHiroya <hiroya19840513@gmail.com>
2018-08-13 19:24:10 +0900

    Add files via upload

--- Func/C5-2_Exe-Ticket.ps1
+++ Func/C5-2_Exe-Ticket.ps1
@@ -10,19 +10,19 @@ $TicketFile|%{
 	ipcsv $_ -encoding default|%{
 	
 		# タイミングによる実施有無を判定
-		if($_.Timing -eq "confirm"){
+		if($_.Timing -eq "Confirm"){
 			#[ValidateSet('Yes','No')]$Answer=Read-Host ($_.Ticket+" を実行しますか？Yes/No")
 			#if($Answer -eq 'No'){return}
 			if((Read-Host ($_.Ticket+" を実行しますか？Yes/No")) -eq 'No'){return}
 		} 
 	
-		if($_.WorkDir){$WorkDirCommand=" -WorkDir "+$_.WorkDir}else{$WorkDirCommand=""}
+		if($_.SrcPath){$SrcPathCommand=" -SrcPath "+$_.SrcPath}else{$SrcPathCommand=""}
 		if($_.InFile){$InCommand=" -infile "+$_.InFile}else{$InCommand=""}
 		if($_.OutFile){$OutCommand=" -outfile "+$_.OutFile}else{$OutCommand=""}
 		if($_.Row){$RowCommand=" -row "+$_.Row}else{$RowCommand=""}
 		if($_.Line){$LineCommand=" -line "+$_.Line}else{$LineCommand=""}
 	
-		Invoke-Expression ($_.CommandName + $WorkDirCommand + $InCommand + $OutCommand + $RowCommand + $LineCommand)
+		Invoke-Expression ($_.CommandName + $SrcPathCommand + $InCommand + $OutCommand + $RowCommand + $LineCommand)
 	
 	}
 }
