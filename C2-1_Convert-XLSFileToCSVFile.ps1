020b24d8964e85546c5c98aa77d0449a4127c7ea
NakamuraHiroya <hiroya19840513@gmail.com>
2018-08-13 19:30:08 +0900

    Add files via upload

---
+++ C2-1_Convert-XLSFileToCSVFile.ps1
@@ -0,0 +1,33 @@
+function global:Convert-XLSFileToCSVFile{
+
+param(
+	$InFile=$(ls -name *.xls*|ogv -PassThru -Title "入力するファイルを指定して下さい。"),
+	$OutFile,
+	$Ticket
+)
+
+$excel=New-Object -ComObject Excel.Application
+$excel.Visible=$false 
+$excel.DisplayAlerts=$false
+
+#エクセルを開く
+$book=$excel.Workbooks.Open((Convert-path $InFile))
+
+foreach($sheet in $book.sheets){
+	$sheet.Activate()
+	$OutFile=(($InFile -split "\.")[0])+"_"+$sheet.Name+".csv"
+
+	#Sheet数が1の場合そのままの名称でCSV保存
+	if((($book.sheets|measure).count) -eq 1){$OutFile=(($InFile -split "\.")[0])+".csv"}
+
+	#Excelを同じフォルダに新規CSV保存
+	$book.SaveAs((Split-Path(Convert-Path $InFile) -parent) + "\" + $OutFile,6)
+
+}
+$excel.Quit();$excel = $null
+
+# チケットログ作成
+$FunctionName=$MyInvocation.MyCommand.Name
+if($Ticket){Create-Ticket -Data "$Ticket,Always,$FunctionName,,$InFile"}
+
+}
