fc96593fa615c0bf37061e08265f67fc38c7e7ca
NakamuraHiroya <hiroya19840513@gmail.com>
2018-11-24 16:54:10 +0900

    Add files via upload

--- _Script/C1-07_Gather-File.ps1
+++ _Script/C1-07_Gather-File.ps1
@@ -1,5 +1,7 @@
 function global:Gather-File{
 
+# 
+
 param(
 	$WorkDir=$((pwd).path),
 	$InFile="_TicketList.csv",
@@ -7,7 +9,7 @@ param(
 	$OutFile="_GatherList.csv",
 	$Ticket
 )
-ls -dir -name $Value|%{if(ls $_ "_TicketList.csv"){(ipcsv (join-path $_ $InFile) -Encoding Default)}}|
+ls -dir -name $Value|%{if(ls $_ $InFile){(ipcsv (join-path $_ $InFile) -Encoding Default)}}|
 ConvertTo-Csv -NoTypeInformation|%{$_.replace('"','')}|Out-File $OutFile -encoding default
 
 # チケットログ作成 "Ticket,Timing,CommandName,WorkDir,InFile,OutFile,Row,Line,Value,SrcDir,SrcPath,DistDir,DistPath"
