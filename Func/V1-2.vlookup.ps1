ea0a59e66febe8a6fefcead371da0acbdf3ae9a0
NakamuraHiroya <hiroya19840513@gmail.com>
2018-11-25 20:22:14 +0900

    Delete V1-2.vlookup.ps1

--- Func/V1-2.vlookup.ps1
+++
@@ -1,7 +0,0 @@
-function global:vlookup($Key,$Master,$MasterKey,$MasterValue){
-
-# $datalist.'FileType_Code.csv'|?{$_.FileTypeCode -eq "_WBS"}|%{$_.SearchWords}
-
-$Datalist.$Master|?{$_.$MasterKey -eq $Key}|%{$_.$MasterValue}
-
-}
